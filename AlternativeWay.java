9da0cecf0a680eb82f7a040ce2312fb4cfb6444f
devika <iamdevkala@gmail.com>
2019-10-17 15:36:47 -0400

    added to the file

--- AlternativeWay.java
+++ AlternativeWay.java
@@ -1,4 +1,10 @@
 public class AlternativeWay {
+
+//let's see if it works for the git changs .
+//this is just for practic purpose for git
+
+
+
         public static void main(String[] args) {
             char ch = 'u';
             switch (ch) {
